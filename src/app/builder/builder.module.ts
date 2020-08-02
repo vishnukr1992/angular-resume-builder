174810cf352fac0be7bbbff3158f825ac5068bcd
Vishnu K R <kr.vishnu401@gmail.com>
2020-08-03 00:04:16 +0530

    router configured

--- src/app/builder/builder.module.ts
+++ src/app/builder/builder.module.ts
@@ -5,11 +5,16 @@ import { MaterialModule } from "../shared/material/material.module";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { BuilderBoardComponent } from "./builder-board/builder-board.component";
+import { BuilderMenuService } from "./service/builder-menu.service";
+import { ObjectiveComponent } from './objective/objective.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 
 @NgModule({
   declarations: [
     BuilderLayoutComponent,
-    BuilderBoardComponent],
+    BuilderBoardComponent,
+    ObjectiveComponent,
+    PersonalDetailsComponent],
   imports: [
     CommonModule,
     BuilderRoutingModule,
@@ -17,7 +22,7 @@ import { BuilderBoardComponent } from "./builder-board/builder-board.component";
     ReactiveFormsModule,
     FormsModule
   ],
-  providers: [],
+  providers: [BuilderMenuService],
   bootstrap: []
 })
 export class BuilderModule {
