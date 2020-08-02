9d17e1a5fb41ee76039b82895f1e6d7f76ae93c6
Vishnu K R <kr.vishnu401@gmail.com>
2020-08-03 01:05:36 +0530

    objective-page design completed

--- src/app/builder/objective/objective.component.ts
+++ src/app/builder/objective/objective.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {ObjectiveModel} from "../model/objective.model";
 
 @Component({
-  selector: 'app-objective',
-  templateUrl: './objective.component.html',
-  styleUrls: ['./objective.component.scss']
+  selector: "app-objective",
+  templateUrl: "./objective.component.html",
+  styleUrls: ["./objective.component.scss"]
 })
 export class ObjectiveComponent implements OnInit {
+
+  public objective: ObjectiveModel = new ObjectiveModel();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public saveObjective() {
+    console.log(this.objective);
   }
 
 }
